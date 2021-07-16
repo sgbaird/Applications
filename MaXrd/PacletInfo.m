0c3826a23fc7f3fc3013be509dbecc9364d405cc
sgbaird <sterling.baird@icloud.com>
2021-07-16 01:04:27 -0600

    MaXrd

---
+++ MaXrd/PacletInfo.m
@@ -0,0 +1,27 @@
+(* Paclet Info File *)
+
+(* created 2020/08/26*)
+
+Paclet[
+    Name -> "MaXrd",
+    Version -> "2.4.0",
+    MathematicaVersion -> "11.3+",
+    Description -> "Database and utilities related to crystallography and X-ray scattering.",
+    Creator -> "Stian Penev Ramsnes",
+    URL -> "https://github.com/Stianpr20/MaXrd",
+    Extensions -> 
+        {
+            {"Application", Context -> "MaXrd`"}, 
+            {"Kernel", Context -> 
+                {"MaXrd`"}
+            }, 
+            {"Documentation", Resources -> 
+                {"Guides/MaXrd"}
+            , Language -> "English", MainPage -> "Guides/MaXrd"}, 
+            {"FrontEnd", Context -> 
+                {"MaXrd"}
+            }
+        }
+]
+
+
---
+++ MaXrd/UserData/CrystalData.m
@@ -0,0 +1,61 @@
+(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
+<|"mp-134" -> <|"FormulaUnits" -> 4, "SpaceGroup" -> 
+    "Fm\!\(\*OverscriptBox[\(3\), \(_\)]\)m", "LatticeParameters" -> 
+    <|"a" -> Quantity[4.03893, "Angstroms"], 
+     "b" -> Quantity[4.03893, "Angstroms"], 
+     "c" -> Quantity[4.03893, "Angstroms"], "\[Alpha]" -> 
+      Quantity[90, "AngularDegrees"], "\[Beta]" -> 
+      Quantity[90, "AngularDegrees"], "\[Gamma]" -> 
+      Quantity[90, "AngularDegrees"]|>, "AtomData" -> 
+    {<|"Element" -> "Al", "FractionalCoordinates" -> {0., 0., 0.}|>}|>, 
+ "mp-22862" -> <|"ChemicalFormula" -> 
+    "\!\(\*SubscriptBox[\(Na\), \(4\)]\)\!\(\*SubscriptBox[\(Cl\), \(4\)]\)", 
+   "FormulaUnits" -> 4, "SpaceGroup" -> "P1", "LatticeParameters" -> 
+    <|"a" -> Quantity[5.691694, "Angstroms"], 
+     "b" -> Quantity[5.691694, "Angstroms"], 
+     "c" -> Quantity[5.691694, "Angstroms"], "\[Alpha]" -> 
+      Quantity[90, "AngularDegrees"], "\[Beta]" -> 
+      Quantity[90, "AngularDegrees"], "\[Gamma]" -> 
+      Quantity[90, "AngularDegrees"]|>, "AtomData" -> 
+    {<|"Element" -> "Na", "FractionalCoordinates" -> {0., 0., 0.}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0., 0.5, 0.5}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0.5, 0., 0.5}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0.5, 0.5, 0.}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0.5, 0., 0.}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0.5, 0.5, 0.5}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0., 0., 0.5}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0., 0.5, 0.}|>}|>, 
+ "test" -> <|"ChemicalFormula" -> 
+    "\!\(\*SubscriptBox[\(Na\), \(4\)]\)\!\(\*SubscriptBox[\(Cl\), \(4\)]\)", 
+   "FormulaUnits" -> 4, "SpaceGroup" -> "P1", "LatticeParameters" -> 
+    <|"a" -> Quantity[5.691694, "Angstroms"], 
+     "b" -> Quantity[5.691694, "Angstroms"], 
+     "c" -> Quantity[5.691694, "Angstroms"], "\[Alpha]" -> 
+      Quantity[90, "AngularDegrees"], "\[Beta]" -> 
+      Quantity[90, "AngularDegrees"], "\[Gamma]" -> 
+      Quantity[90, "AngularDegrees"]|>, "AtomData" -> 
+    {<|"Element" -> "Na", "FractionalCoordinates" -> {0., 0., 0.}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0., 0.5, 0.5}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0.5, 0., 0.5}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0.5, 0.5, 0.}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0.5, 0., 0.}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0.5, 0.5, 0.5}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0., 0., 0.5}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0., 0.5, 0.}|>}|>, 
+ "TestInput" -> <|"ChemicalFormula" -> 
+    "\!\(\*SubscriptBox[\(Na\), \(4\)]\)\!\(\*SubscriptBox[\(Cl\), \(4\)]\)", 
+   "FormulaUnits" -> 4, "SpaceGroup" -> "P1", "LatticeParameters" -> 
+    <|"a" -> Quantity[5.691694, "Angstroms"], 
+     "b" -> Quantity[5.691694, "Angstroms"], 
+     "c" -> Quantity[5.691694, "Angstroms"], "\[Alpha]" -> 
+      Quantity[90, "AngularDegrees"], "\[Beta]" -> 
+      Quantity[90, "AngularDegrees"], "\[Gamma]" -> 
+      Quantity[90, "AngularDegrees"]|>, "AtomData" -> 
+    {<|"Element" -> "Na", "FractionalCoordinates" -> {0., 0., 0.}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0., 0.5, 0.5}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0.5, 0., 0.5}|>, 
+     <|"Element" -> "Na", "FractionalCoordinates" -> {0.5, 0.5, 0.}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0.5, 0., 0.}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0.5, 0.5, 0.5}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0., 0., 0.5}|>, 
+     <|"Element" -> "Cl", "FractionalCoordinates" -> {0., 0.5, 0.}|>}|>|>
